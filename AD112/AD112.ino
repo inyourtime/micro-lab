a43bb168862cbc6458e4ee78927289d4939636df
sahachai <inyourtimeguy@gmail.com>
2021-04-22 11:31:11 +0700

    change to functional

--- AD112/AD112.ino
+++ AD112/AD112.ino
@@ -9,7 +9,44 @@ uint8_t digit[16] = { 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xD8,
                       0x80, 0x90, 0x88, 0x83, 0xA7, 0xA1, 0x86, 0x8E  
                     };
 
-void reset() {
+void _crossTheRoad() {
+    
+    for (int i = 5; i >= 0; --i) {
+        PORTA = digit[i];
+        if (i == 0) {
+           _preStop();
+           _stop(); 
+        }
+        delay(1000);
+    }
+
+    
+}
+
+void _preStop() {
+    
+    digitalWrite(ledGrn, HIGH);
+    digitalWrite(ledYel, LOW);
+    delay(3000); 
+
+}
+
+void _stop() {
+
+    for (int i = 9; i >= 0; --i) {
+        digitalWrite(ledYel, HIGH);
+        digitalWrite(ledRed, LOW);
+        PORTA = digit[i];
+        delay(1000);
+
+        if (i == 0) {
+            _reset();
+        }
+    }
+    
+}
+
+void _reset() {
     
     PORTA = 0xFF;
     digitalWrite(ledYel, HIGH);
@@ -30,6 +67,7 @@ void setup() {
     digitalWrite(ledYel, HIGH);
     digitalWrite(ledRed, HIGH);
     digitalWrite(ledGrn, LOW);
+    
     Serial.begin(9600);
     
 }
@@ -40,24 +78,7 @@ void loop() {
     Serial.println(stateStart);
 
     if (stateStart == 0) {
-        for (int i = 5; i >= 0; --i) {
-            PORTA = digit[i];
-            if (i == 0) {
-                digitalWrite(ledGrn, HIGH);
-                digitalWrite(ledYel, LOW);
-                delay(3000);
-                for (int i = 9; i >= 0; --i) {
-                    digitalWrite(ledYel, HIGH);
-                    digitalWrite(ledRed, LOW);
-                    PORTA = digit[i];
-                    delay(1000);
-                    if (i == 0) {
-                        reset();
-                    }
-                }
-            }
-            delay(1000);
-        }
+        _crossTheRoad();
     }
     
 }
