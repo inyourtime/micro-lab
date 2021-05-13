eaabd75a7beecdbf2d51ca0737171c29753c71c6
sahachai <inyourtimeguy@gmail.com>
2021-05-13 12:05:29 +0700

    change format

--- test_adc/lib/adcSetup/adc_setup.cpp
+++ test_adc/lib/adcSetup/adc_setup.cpp
@@ -1,37 +1,26 @@
-#include <avr/io.h>
 #include "adc_setup.h"
+#include <avr/io.h>
 
-void adcSetup(unsigned int ch)
-{
+void adcSetup(unsigned int ch) {
     ADCSRA = 0x97;
     ADCSRB = 0;
     ADMUX = 0x40;
-    if (ch < 8)
-    {
-    	ADMUX |= ch;
-    }
-    else
-    {
-    	ADCSRB |= 0x8;
-    	ADMUX |= (ch & 0x7);
+    if (ch < 8) {
+        ADMUX |= ch;
+    } else {
+        ADCSRB |= 0x8;
+        ADMUX |= (ch & 0x7);
     }
 }
 
-int adcChangeChannel(unsigned int ch)
-{
-	int output = 1;
-	if (ch > 15)
-	{
-		output = 0;
-	}
-	else if (ch < 8)
-	{
-		ADMUX |= 0x8;
-		ADMUX &= ~(0x1F);
-		ADMUX |= (ch & 0x7);
-	}
-	return (output);
+int adcChangeChannel(unsigned int ch) {
+    int output = 1;
+    if (ch > 15) {
+        output = 0;
+    } else if (ch < 8) {
+        ADMUX |= 0x8;
+        ADMUX &= ~(0x1F);
+        ADMUX |= (ch & 0x7);
+    }
+    return (output);
 }
-
-
-
