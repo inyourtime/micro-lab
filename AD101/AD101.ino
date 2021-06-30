927b367dbe2f9e1dbbdeeefe1586236208435019
sahachai <inyourtimeguy@gmail.com>
2021-06-30 10:50:20 +0700

    modified

--- AD101/AD101.ino
+++ AD101/AD101.ino
@@ -4,6 +4,7 @@ uint8_t digit[16] = { 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8,
                     };
 
 int count = 0;
+const int t1 = 100;
 
 void setup() {
     // Led red yellow green
@@ -12,61 +13,107 @@ void setup() {
 
     // 7 segments
     DDRC |= 0xFF;
-//    PORTC = digit[0];
-    PORTC |= 0xFF; 
+    PORTC = digit[0];
+//    PORTC |= 0xFF; 
 
     // 8 led module
-    DDRF |= 0xFF;
-    PORTF |= 0xFF;
-
+    DDRK |= 0xFF;
+    PORTK |= 0xFF;
 }
 
 void loop() {
 
-    // led traffic light
+//    task1();    // led traffic light module
+    
+//    task2();    // 7 segments module
+
+    task3();    // my pattern
+
+//    pattern1();
+
+//    pattern2();
+
+//    pattern3();
+ 
+}
+
+void task1() {
     for (int i = 0; i < 3; i++) {
         PORTA = ~(1 << i);
         delay(1000);
     }
+}
 
-    // 7 segments
-//    for (int i = 0; i < 16; i++) {
-//        PORTC = digit[i];
-//        delay(1000);
-//    }
+void task2() {
+    for (int i = 0; i < 16; i++) {
+        PORTC = digit[i];
+        delay(1000);
+    }
+}
 
-    // 8 led module
-//    for (int i = 0; i < 8; i++) {
-//        PORTF = ~(1 << i);
-//        delay(200);
-//        if (i == 7) {
-//            if (count == 15) count = -1;
-//            PORTC = digit[count + 1];
-//            count += 1;
-//        }
-//    }
+void task3() {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8-i; j++) {
+            PORTK = ~(1 << j+i);
+            for (int k = 0; k < i; k++) {
+                PORTK &= ~(1 << k);
+            }
+            delay(t1);
+        }
+        for (int j = 6; j > i-1; j--) {
+            PORTK = ~(1 << j);
+            for (int k = 0; k < i; k++) {
+                PORTK &= ~(1 << k);
+            } 
+            delay(t1);
+        }
+    }
+    for (int i = 7; i >= 0; i--) {
+        PORTK |= (1 << i);
+        delay(t1);
+    }
+}
 
-//    for (int i = 0; i < 8; i++) {
-//        PORTF = ~(1 << i);
-//        delay(200);
-//        if (i == 7) {
-//            for (int j = 6; j > 0; j--) {
-//                PORTF = ~(1 << j);
-//                delay(200);
-//            }
-//        }
-//    }
+void pattern1() {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (j > i) {
+                PORTK = 0xFF;
+                delay(t1);
+                break; 
+            }
+            PORTK &= ~(1 << j);
+            delay(t1);
+            if (j == 7) {
+                for (int k = 7; k >= 0; k--) {
+                    PORTK |= (1 << k);
+                    delay(t1);
+                    if (k == 0) {
+                        if (count == 15) count = -1;
+                        PORTC = digit[count + 1];
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+}
 
-//    for (int i = 0; i < 4; i++) {
-//        PORTF = ~((1 << i) | (1 << 7-i));
-//        delay(200);
-//        if (i == 3) {
-//            for (int j = 2; j > 0; j--) {
-//                PORTF = ~((1 << j) | (1 << 7-j));
-//                delay(200);
-//            }
-//        }
-//    }
+void pattern2() {
+    for (int i = 0; i < 8; i++) {
+        PORTK = ~(1 << i);
+        delay(200);
+        if (i == 7) {
+            if (count == 15) count = -1;
+            PORTC = digit[count + 1];
+            count += 1;
+        }
+    }
+}
 
-
+void pattern3() {
+    for (int i = 0; i < 10; i++) {
+        PORTK = ~((1 << i) | (1 << i-1) | (1 << i-2));
+        delay(t1);
+    }
 }
